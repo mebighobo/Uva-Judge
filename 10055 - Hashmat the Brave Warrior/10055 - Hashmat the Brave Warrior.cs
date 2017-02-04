f5e47badd39e8c3323a51a1565069cadabe80a42
mebighobo <john_chang69@hotmail.com>
2017-02-04 13:45:52 -0800

    Add files via upload

---
+++ 10055 - Hashmat the Brave Warrior/10055 - Hashmat the Brave Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("text.txt");
+            
+            foreach (String lines1 in lines)
+            {
+                int a, b, c;
+                String[] word = lines1.Split();
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    a = Int32.Parse(word[i]);
+
+                    for (int j = 1; j < word.Length; j++)
+                    {
+                        b = Int32.Parse(word[j]);
+
+                        if (b > a)
+                        {
+                            c = b - a;
+                            Console.WriteLine(c);
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
